c7beb5dcedf5522c5998bebde48ab7e073ddf9b5
Kevin Old <kevin@kevinold.com>
2023-11-13 15:47:04 -0600

    wip - overrides

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,7 +1,14 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+  // override: {
+  //   userPool: {
+  //     passwordPolicy: {
+  //       temporaryPasswordValidityDays: 3,
+  //     },
+  //   },
+  // },
 });
--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,6 @@ import { defineBackend } from "@aws-amplify/backend";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { auth } from "./auth/resource.js";
 import { CustomNotifications } from "./custom/CustomNotifications/resource.js";
-import { LambdaInAVpcStack } from "./custom/LambdaInAVpcStack/resource.js";
 import { data } from "./data/resource.js";
 
 const backend = defineBackend({
@@ -10,13 +9,23 @@ const backend = defineBackend({
   data,
 });
 
+// backend.resources.auth.resources.cfnResources.userPool.addPropertyOverride(
+//   "UserPoolTags",
+//   {
+//     Key: "userpool-tag-2",
+//     Value: "userpool-tag-value-2",
+//   }
+// );
+
+// backend.resources.auth.resources.userPool.passwordPolicy.temporaryPasswordValidityDays = 3;
+
 // create the bucket and its stack
 const bucketStack = backend.getStack("BucketStack");
 const bucket = new s3.Bucket(bucketStack, "Bucket", {
   blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
 });
 
-new LambdaInAVpcStack(backend.getStack("LambdaInAVpcStack"), "LambdaInAVpc");
+//new LambdaInAVpcStack(backend.getStack("LambdaInAVpcStack"), "LambdaInAVpc");
 
 //new BackupStack(backend.getStack('BackupStack'),
 // 'Backup', { database: backend.resources.data.resources.database })
