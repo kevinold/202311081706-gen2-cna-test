e7ae0282bbe9a270214b778bfa1ea77b385fbb0e
Kevin Old <kevin@kevinold.com>
2023-11-14 13:32:34 -0600

    add examples of oidc and allow list roles

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,30 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { a, defineData, type ClientSchema } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  Todo: a.model({
-    name: a.string(),
-    description: a.string(),
-  }),
+  Todo: a
+    .model({
+      name: a.string(),
+      description: a
+        .string()
+        .authorization([a.allow.public("iam").to(["read"])]),
+    })
+    .authorization([a.allow.public("iam").to(["read"]), a.allow.owner()]),
 });
+
+// export const data = defineData({
+//   schema,
+// authorizationModes: {
+//   oidcAuthorizationMode: {
+//     oidcProviderName: "oidc-provider-name",
+//     oidcIssuerUrl: "https://example.com",
+//     clientId: "client-id",
+//     tokenExpiryFromAuthInSeconds: 300,
+//     tokenExpireFromIssueInSeconds: 600,
+//   },
+//   //IAM Role names which are provided full r/w access to the API for models with IAM authorization.
+//   allowListedRoleNames: ["arn:aws:iam::<account-id>:role/<role-name>"],
+// },
+//});
 
 export type Schema = ClientSchema<typeof schema>;
 
