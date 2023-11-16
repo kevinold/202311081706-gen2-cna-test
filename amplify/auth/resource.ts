9284bf26e89552e792ad4b26c9610aa136d7581d
Kevin Old <kevin@kevinold.com>
2023-11-16 15:36:29 -0600

    custom email verification settings

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -2,6 +2,11 @@ import { defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailBody: (code: string) =>
+        `Welcome! Your verification code is ${code}.`,
+      verificationEmailSubject: "Welcome! Here is your verification code",
+    },
   },
 });
